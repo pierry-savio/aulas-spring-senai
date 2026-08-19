27602277e437e5762f0e21ad6e518be370939989
pierry <pierrysavio.pi@gmail.com>
2026-08-19 11:53:35 -0300

    feat: users crud api rest

---
+++ src/main/java/com/pierry/calculator/User.java
@@ -0,0 +1,16 @@
+package com.pierry.calculator;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String name;
+    private String cpf;
+    private String email;
+}
---
+++ src/main/java/com/pierry/calculator/UserController.java
@@ -0,0 +1,37 @@
+package com.pierry.calculator;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    public ArrayList<User> users = new ArrayList<>();
+
+    @GetMapping
+    public ArrayList<User> listAllUsers() {
+        return users;
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user){
+        users.add(user);
+        return users.getLast();
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User user){
+        User updatedUser = users.get(id);
+        updatedUser.setName(user.getName());
+        updatedUser.setCpf(user.getCpf());
+        updatedUser.setEmail(user.getEmail());
+        return updatedUser;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable int id){
+        users.remove(id);
+    }
+}
